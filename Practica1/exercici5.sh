74c67273c3ee1965e9cee70199037b3b2c1fde13
Eudaldz <eudald.elias@gmail.com>
2018-03-06 17:18:41 +0100

    update

--- Practica1/exercici5.sh
+++ Practica1/exercici5.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #falta testejar.
 
 if [ $# -ne 3 ]
@@ -11,14 +12,18 @@ else
     
     for file in $(ls $dir); do
         if [ -f "$dir/$file" ]; then
-            if [ $file == *".ext" ]; then
-                for word in &(cat $file)
-                    if [ $word == $cadena  ]; then
+            if [[ $file == *".$ext" ]]; then
+                for word in $(cat $dir/$file); do
+                    if [ $word == $string ]; then
                         echo "$dir/$file"
                         break
+                    fi
+                done
             fi
         else
-            if [ -d "$dir/$file" ]
+            if [ -d "$dir/$file" ]; then
+                ./exercici5.sh $dir/$file $ext $string
+            fi
         fi
     done
     exit 0
