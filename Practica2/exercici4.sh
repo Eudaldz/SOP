495ee76c11a83f3e828e77dc6e61f0ebe6042cee
Eudaldz <eudald.elias@gmail.com>
2018-03-15 12:56:07 +0100

    Add files via upload

---
+++ Practica2/exercici4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# -ne 3 ]
+then
+	echo "nombre paramatres incorrecte"    
+	exit 1
+else
+    	ext=$1
+	c1=$2
+	c2=$3
+	contar_paraula="echo {} \$(grep -o $c1 {} | wc -l); sed -n 's/$c1/$c2/g' {}"
+	#echo $contar_paraula
+	find . -name "*.$ext" -type f -exec bash -c "$contar_paraula" \;	
+    	
+	exit 0
+    
+fi
