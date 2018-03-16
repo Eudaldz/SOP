a092720ca6bf901e41778bbc6a85d5d875ebb514
Guillem Molina Galera <guillem_3_8@hotmail.com>
2018-03-16 13:41:52 +0100

    Update exercici4.sh

--- Practica2/exercici4.sh
+++ Practica2/exercici4.sh
@@ -8,8 +8,7 @@ else
     	ext=$1
 	c1=$2
 	c2=$3
-	contar_paraula="echo {} \$(grep -o $c1 {} | wc -l); sed -i -- 's/$c1/$c2/g' {}"
-	#echo $contar_paraula
+	contar_paraula="echo {} \$(grep -o $c1 {} | wc -l); sed -i -- 's/$c1/$c2/g' {}" #codi a executar en el find
 	find . -name "*.$ext" -type f -exec bash -c "$contar_paraula" \;	
     	
 	exit 0
